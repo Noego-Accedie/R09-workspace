e80490874731bf7836e8a8336a90c0ec33e6778b
Noego-Accedie <noego.accedie@outlook.com>
2023-09-15 10:48:39 +0800

    Changed variable names, GPIO assignment

--- n20encoder3.py
+++ n20encoder3.py
@@ -8,11 +8,11 @@ from PCA9685 import PCA9685
 from AlphaBot2 import AlphaBot2
 
 # Motor control pins of DRV8833
-IN1 = 20
-IN2 = 16
+motor1 = 19
+motor2 = 24
 # Encoder pins
-C1 = 23
-C2 = 24
+encoder1 = 21
+encoder2 = 23
 
 # Alphabot motor object
 Ab = AlphaBot2()
@@ -25,48 +25,48 @@ screen.keypad(True)
 speed = 30
 
 # Encoder pulses counter
-C1count = 0
-C2count = 0
+encoder1Count = 0
+encoder2Count = 0
 
     
-# Interrupt routine when encoder pin C1 has event
+# Interrupt routine when encoder pin encoder1 has event
 # To be assigned in main program
-def c1_callback(channel):
-    global C1count
-    C1count += 1
-    print("C1 event: ", C1count)
+def encoder1_callback(channel):
+    global encoder1Count
+    encoder1Count += 1
+    print("encoder1 event: ", encoder1Count)
     
-# Interrupt routine when encoder pin C2 has event
+# Interrupt routine when encoder pin encoder2 has event
 # To be assigned in main program
-def c2_callback(channel):
-    global C2count
-    C2count += 1
-    print("C2 event: ", C2count)
+def encoder2_callback(channel):
+    global encoder2Count
+    encoder2Count += 1
+    print("encoder2 event: ", encoder2Count)
 
 
 try:
-    GPIO.setmode(GPIO.BCM)
+    GPIO.setmode(GPIO.BOARD)
     # Setup GPIO input and output
-    GPIO.setup(C1, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Encoder C1 pin
-    GPIO.setup(C2, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Encoder C2 pin
-    GPIO.setup(IN1, GPIO.OUT) # Conneted to AIN2 of DRV8833
-    GPIO.setup(IN2, GPIO.OUT) # Conneted to AIN2 of DRV8833
+    GPIO.setup(encoder1, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Encoder encoder1 pin
+    GPIO.setup(encoder2, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Encoder encoder2 pin
+    GPIO.setup(motor1, GPIO.OUT) # Conneted to AIN2 of DRV8833
+    GPIO.setup(motor2, GPIO.OUT) # Conneted to AIN2 of DRV8833
 
     # Set both motor pins LOW at the beginning
     # Ensure motor stop
-    GPIO.output(IN1, GPIO.LOW) # Set AIN1
-    GPIO.output(IN2, GPIO.LOW) # Set AIN1
+    GPIO.output(motor1, GPIO.LOW) # Set AIN1
+    GPIO.output(motor2, GPIO.LOW) # Set AIN1
 
     # Set up PWM for the pins
     # Both pins from one motor are used for both direction
     # This is specific to DRV8833, the driver in Alphabot will be different
-    PWMA1 = GPIO.PWM(IN1, 50) # Use 500Hz
-    PWMA2 = GPIO.PWM(IN2, 50) # Use 500Hz
+    PWM1 = GPIO.PWM(motor1, 50) # Use 500Hz
+    PWM2 = GPIO.PWM(motor2, 50) # Use 500Hz
 
-    GPIO.add_event_detect(C1, GPIO.RISING, callback=c1_callback, bouncetime=100) # Setup callback for C1 rising pulse detected, bouncetime is for debouncing
-    GPIO.add_event_detect(C2, GPIO.RISING, callback=c2_callback, bouncetime=100) # Setup callback for C2 rising pulse detected, bouncetime is for debouncing
-    #GPIO.add_event_detect(C1, GPIO.RISING, callback=c1_callback) # Setup callback for C1 rising pulse detected
-    #GPIO.add_event_detect(C2, GPIO.RISING, callback=c2_callback) # Setup callback for C2 rising pulse detected
+    GPIO.add_event_detect(encoder1, GPIO.RISING, callback=encoder1_callback, bouncetime=100) # Setup callback for encoder1 rising pulse detected, bouncetime is for debouncing
+    GPIO.add_event_detect(encoder2, GPIO.RISING, callback=encoder2_callback, bouncetime=100) # Setup callback for encoder2 rising pulse detected, bouncetime is for debouncing
+    #GPIO.add_event_detect(encoder1, GPIO.RISING, callback=c1_callback) # Setup callback for encoder1 rising pulse detected
+    #GPIO.add_event_detect(encoder2, GPIO.RISING, callback=c2_callback) # Setup callback for encoder2 rising pulse detected
 
     while True:
         char = screen.getch()
@@ -84,12 +84,12 @@ try:
             Ab.stop()
 
             
-        print("C1 event: ", C1count)
-        print("C2 event: ", C2count)
+        print("encoder1 event: ", encoder1Count)
+        print("encoder2 event: ", encoder2Count)
 
 
 finally:
-    curses.nocbreak(); screen.keypad(0); curses.echo(0); curses.endwin(); GPIO.remove_event_detect(C1); GPIO.remove_event_detect(C2); GPIO.cleanup()
+    curses.nocbreak(); screen.keypad(0); curses.echo(0); curses.endwin(); GPIO.remove_event_detect(encoder1); GPIO.remove_event_detect(encoder2); GPIO.cleanup()
 
 
 
@@ -106,41 +106,41 @@ if __name__ == '__main__':
     
     GPIO.setmode(GPIO.BCM)
     # Setup GPIO input and output
-    GPIO.setup(C1, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Encoder C1 pin
-    GPIO.setup(C2, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Encoder C2 pin
-    GPIO.setup(IN1, GPIO.OUT) # Conneted to AIN2 of DRV8833
-    GPIO.setup(IN2, GPIO.OUT) # Conneted to AIN2 of DRV8833
+    GPIO.setup(encoder1, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Encoder encoder1 pin
+    GPIO.setup(encoder2, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Encoder encoder2 pin
+    GPIO.setup(motor1, GPIO.OUT) # Conneted to AIN2 of DRV8833
+    GPIO.setup(motor2, GPIO.OUT) # Conneted to AIN2 of DRV8833
 
     # Set both motor pins LOW at the beginning
     # Ensure motor stop
-    GPIO.output(IN1, GPIO.LOW) # Set AIN1
-    GPIO.output(IN2, GPIO.LOW) # Set AIN1
+    GPIO.output(motor1, GPIO.LOW) # Set AIN1
+    GPIO.output(motor2, GPIO.LOW) # Set AIN1
 
     # Set up PWM for the pins
     # Both pins from one motor are used for both direction
     # This is specific to DRV8833, the driver in Alphabot will be different
-    PWMA1 = GPIO.PWM(IN1, 500) # Use 500Hz
-    PWMA2 = GPIO.PWM(IN2, 500) # Use 500Hz
+    PWM1 = GPIO.PWM(motor1, 500) # Use 500Hz
+    PWM2 = GPIO.PWM(motor2, 500) # Use 500Hz
     
-    # GPIO.add_event_detect(C1, GPIO.RISING, callback=c1_callback, bouncetime=100) # Setup callback for C1 rising pulse detected, bouncetime is for debouncing
-    # GPIO.add_event_detect(C2, GPIO.RISING, callback=c2_callback, bouncetime=100) # Setup callback for C2 rising pulse detected, bouncetime is for debouncing
-    GPIO.add_event_detect(C1, GPIO.RISING, callback=c1_callback) # Setup callback for C1 rising pulse detected
-    GPIO.add_event_detect(C2, GPIO.RISING, callback=c2_callback) # Setup callback for C2 rising pulse detected
+    # GPIO.add_event_detect(encoder1, GPIO.RISING, callback=c1_callback, bouncetime=100) # Setup callback for encoder1 rising pulse detected, bouncetime is for debouncing
+    # GPIO.add_event_detect(encoder2, GPIO.RISING, callback=c2_callback, bouncetime=100) # Setup callback for encoder2 rising pulse detected, bouncetime is for debouncing
+    GPIO.add_event_detect(encoder1, GPIO.RISING, callback=c1_callback) # Setup callback for encoder1 rising pulse detected
+    GPIO.add_event_detect(encoder2, GPIO.RISING, callback=c2_callback) # Setup callback for encoder2 rising pulse detected
     
     # signal.signal(signal.SIGINT, signal_handler) # Setup callback for Ctrl+C
     # signal.pause()
     
     # Drive the motor clockwise
-    PWMA1.stop() # Start PWM at his pin at 50% duty cycle
-    PWMA2.start(12)
+    PWM1.stop() # Start PWM at his pin at 50% duty cycle
+    PWM2.start(12)
     # Use PWM.ChangeDutyCycle(duty cycle) if we want to change the speed in between
 
     # Wait 5 seconds
     time.sleep(5)
 
     # Stop the motor
-    PWMA1.stop()
-    PWMA2.stop()
+    PWM1.stop()
+    PWM2.stop()
 
     GPIO.cleanup()
     sys.exit(0)
